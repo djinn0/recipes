3c8d1c335146bba5f4c26c06805c73fa6009b41d
djinn0 <dsr.1991@gmail.com>
2020-07-17 15:06:49 -0400

    linked file to generate nav bar

--- navigationbar.js
+++ navigationbar.js
@@ -1,4 +1,13 @@
-function hhh(){
+function generateNavBar(){
+    /* A div with an id of "test" should be added as the first element of the <body> to generate a nav bar at the top of the website */
     var x = document.getElementById("test");
-    x.insertAdjacentHTML('afterbegin', '<ul class="navigation-bar"><li class="navigation-element"><a href="index.html">Home</a></li><li class="navigation-element"><a href="new.html">New Recipes</a></li><li class="dropdown"><a href="#" class="dropbtn">Dropdown</a><div class="dropdown-content"><a href="list.html">Recipe Index</a><a href="glossary.html">Glossary</a><a href="#">Link 3</a></div></ul>')
+    const n1 = '<li class="navigation-element"><a href="index.html">Home</a>'; /*1st element of Navigation bar */
+    const n2 = '</li><li class="navigation-element"><a href="new.html">New Recipes</a></li>' /*2nd element of Navigation bar */
+    /*Additional Navigation bar elements can be added here */
+    /*<li class="dropdown"><a href="#" class="dropbtn">Dropdown</a><div class="dropdown-content"></div>*/
+    const d1 = '<a href=list.html">Recipe Index</a>';
+    const d2 = '<a href="glossary.html">Glossary</a>';
+    const d3 = '<a href="#">Link 3</a>';
+
+    x.insertAdjacentHTML('afterbegin', '<ul class="navigation-bar">' + n1 + n2 + '<li class="dropdown"><a href="#" class="dropbtn">Menu</a><div class="dropdown-content">' + d1 + d2 + d3 + '</div></ul>')
     }
