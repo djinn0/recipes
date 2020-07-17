468be01080fe3ee78953bfc155fd63cf9ac86d88
djinn0 <dsr.1991@gmail.com>
2020-07-17 15:19:58 -0400

    streamlined function to generate nav bar

--- navigationbar.js
+++ navigationbar.js
@@ -1,11 +1,12 @@
 function generateNavBar(){
-    /* A div with an id of "test" should be added as the first element of the <body> to generate a nav bar at the top of the website */
-    var x = document.getElementById("test");
+    /* A function that generates the HTML for a navigation bar on a webpage */
+    /*<div id="navBarLoc"></div> and <script>generateNavBar();</script> should be added as the first elements in <body> */
+    var x = document.getElementById("navBarLoc");
     const n1 = '<li class="navigation-element"><a href="index.html">Home</a>'; /*1st element of Navigation bar */
     const n2 = '</li><li class="navigation-element"><a href="new.html">New Recipes</a></li>' /*2nd element of Navigation bar */
     /*Additional Navigation bar elements can be added here */
     /*<li class="dropdown"><a href="#" class="dropbtn">Dropdown</a><div class="dropdown-content"></div>*/
-    const d1 = '<a href=list.html">Recipe Index</a>';
+    const d1 = '<a href="list.html">Recipe Index</a>';
     const d2 = '<a href="glossary.html">Glossary</a>';
     const d3 = '<a href="#">Link 3</a>';
 
