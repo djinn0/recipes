1436f2e44538a86534e978ce57c34d77cad90f81
djinn0 <dsr.1991@gmail.com>
2020-07-17 14:38:46 -0400

    testing using js to generate navbar

--- navigationbar.js
+++ navigationbar.js
@@ -1,1 +1,4 @@
-
+function hhh(){
+    var x = document.getElementById("test");
+    x.insertAdjacentHTML('afterbegin', '<ul class="navigation-bar"><li class="navigation-element"><a href="index.html">Home</a></li><li class="navigation-element"><a href="new.html">New Recipes</a></li><li class="dropdown"><a href="#" class="dropbtn">Dropdown</a><div class="dropdown-content"><a href="list.html">Recipe Index</a><a href="glossary.html">Glossary</a><a href="#">Link 3</a></div></ul>')
+    }
