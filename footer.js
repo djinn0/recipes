896ab5044f3609ac94edc34708492ba2e1a80333
djinn0 <dsr.1991@gmail.com>
2020-07-17 23:16:14 -0400

    updates

--- footer.js
+++ footer.js
@@ -1,1 +1,15 @@
-
+function generateFooter()
+{
+    /* A function that generates the HTML for a footer on a webpage */
+    /* <script src="footer.js"></script> should be added to the <head> */
+    /* <div id="footerLoc"></div> and <script>generateFooter();</script> should be added as the last elements in <body> */
+    var y = document.getElementById("footerLoc");
+    var d = new Date();
+    var n = d.getFullYear();
+    y.insertAdjacentHTML('afterbegin',`<footer id="footer">© David Redd ${n}</footer>`);
+    document.getElementById("footer").style.backgroundColor = "#333";
+    document.getElementById("footer").style.color = "white";
+    document.getElementById("footer").style.padding = "5px";
+    document.getElementById("footer").style.display = "block";
+    document.getElementById("footer").style.bottom = "0";
+}
--- navigationbar.js
+++ navigationbar.js
@@ -1,6 +1,7 @@
 function generateNavBar(){
     /* A function that generates the HTML for a navigation bar on a webpage */
-    /*<div id="navBarLoc"></div> and <script>generateNavBar();</script> should be added as the first elements in <body> */
+    /* <script src="navigationbar.js"></script> should be added to the <head> */
+    /* <div id="navBarLoc"></div> and <script>generateNavBar();</script> should be added as the first elements in <body> */
     var x = document.getElementById("navBarLoc");
     const n1 = '<li class="navigation-element"><a href="index.html">Home</a>'; /*1st element of Navigation bar */
     const n2 = '</li><li class="navigation-element"><a href="new.html">New Recipes</a></li>' /*2nd element of Navigation bar */
@@ -10,4 +11,16 @@ function generateNavBar(){
     const d2 = '<a href="glossary.html">Glossary</a>';
     const d3 = '<a href="#">Link 3</a>';
     x.insertAdjacentHTML('afterbegin', '<ul class="navigation-bar">' + n1 + n2 + '<li class="dropdown"><a href="#" class="dropbtn">Menu</a><div class="dropdown-content">' + d1 + d2 + d3 + '</div></ul><br>')
-    }
+    
+    /*
+    document.getElementsByClassName("navigation-bar").style.listStyleType = "none";
+    document.getElementsByClassName("navigation-bar").style.margin = "0"; 
+    document.getElementsByClassName("navigation-bar").style.padding = "0";
+    document.getElementsByClassName("navigation-bar").style.backgroundColor = "red";
+    document.getElementsByClassName("navigation-bar").style.position = "fixed";
+    document.getElementsByClassName("navigation-bar").style.top = "0";
+    document.getElementsByClassName("navigation-bar").style.width = "100%";
+    */
+
+
+}
