60122f09ad638f6392656e3e438bdb60ba967a15
djinn0 <dsr.1991@gmail.com>
2020-07-23 16:58:30 -0400

    updates

--- footer.js
+++ footer.js
@@ -3,27 +3,25 @@ function generateFooter()
     /* A function that generates the HTML for a footer on a webpage */
     /* <script src="footer.js"></script> should be added to the <head> */
     /* <div id="footerLoc"></div> and <script>generateFooter();</script> should be added as the last elements in <body> */
-    var x = document.getElementById("footerLoc");
-    var d = new Date();
-    var n = d.getFullYear(); /* Adds current year to footer */
-    x.insertAdjacentHTML('afterbegin',`<ul class="footer-bar" id="fb"><li class="footer-element"><a class="flink" href="https://www.linkedin.com/in/david-redd">&copy David Redd ${n}</a></li><li class="footer-element">About this</li></ul>`);
+    let x = document.getElementById("footerLoc");
+    let d = new Date();
+    let n = d.getFullYear(); /* Adds current year to footer */
+    let f1 = `<li class="footer-element"><a href="https://www.linkedin.com/in/david-redd">&copy David Redd ${n}</a></li>`
+    let f2 = '<li class="footer-element"><a href="aboutme.html">About</a></li>'
+    let f3 = '<li class="footer-element"><a href="aboutme.html">Contact</a></li>'
+    x.insertAdjacentHTML('afterbegin','<ul class="footer-bar" id="fb">' + f1 + f2 + f3 + '</ul>');
     
     /*styling for footer*/
-    var f = document.getElementById("fb");
+    let f = document.getElementById("fb");
     f.style.backgroundColor="#333";
     f.style.color = "white";
     f.style.padding = "0px";
     f.style.display = "block";
     f.style.bottom = "0";
-    f.style.width = "100%"
+    f.style.width = "100%";
 
-    /*
-    var a = f.getElementsByClassName("flink");
-    a[0].color = "inherit"
-    */
-   
     
-    var fb = document.getElementById("fb");
+    let fb = document.getElementById("fb");
     fb.style.listStyleType = "none"
     
       /*
--- navigationbar.js
+++ navigationbar.js
@@ -4,7 +4,7 @@ function generateNavBar(){
     /* <div id="navBarLoc"></div> and <script>generateNavBar();</script> should be added as the first elements in <body> */
     var x = document.getElementById("navBarLoc");
     const n1 = '<li class="navigation-element"><a href="index.html">Home</a>'; /*1st element of Navigation bar */
-    const n2 = '</li><li class="navigation-element"><a href="new.html">New Recipes</a></li>' /*2nd element of Navigation bar */
+    const n2 = '<li class="navigation-element"><a href="new.html">New Recipes</a></li>' /*2nd element of Navigation bar */
     /*Additional Navigation bar elements can be added here */
     /*<li class="dropdown"><a href="#" class="dropbtn">Dropdown</a><div class="dropdown-content"></div>*/
     const d1 = '<a href="list.html">Recipe Index</a>';
