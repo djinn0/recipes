4bfb6e0fac54e2b6d2fdeee64d8caf4b91719d8c
djinn0 <dsr.1991@gmail.com>
2020-09-25 18:16:24 -0400

    footer is positioned dynamicly

--- footer2.js
+++ footer2.js
@@ -1,3 +1,10 @@
+
+window.addEventListener('load', (event) => { 
+    generateFooter();
+});
+
+
+
 function generateFooter()
 {
     var x = document.getElementsByTagName("body")[0];
@@ -9,13 +16,22 @@ function generateFooter()
     x.insertAdjacentHTML('beforeend','<ul class="footer-bar" id="fb">' + f1 + f2 + f3 + '</ul>');
 
     /* Styling of Footer Bar (sf = Styling Footer) */
+  
     
     var s = document.getElementsByTagName("style")[0];
-    var sf1 = '.footer-bar{ list-style-type: none; margin: 0; padding: 0; background-color: #333; width: 100%; position: relative; top: 50%;}';
-    var sf2 = '.footer-element{float: left;}'
-    var sf3 = '.footer-element a{display: inline-block; padding: 10px 6px; text-decoration: none; color: white; }'
-    var sf4 = ' .footer-element a:hover{ background-color: #111}'
+    var contentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.body.clientHeight);
+    var screenHeight = screen.height; 
+    if (contentHeight > screenHeight){
+        var sf1 ='.footer-bar{list-style-type: none; margin: 0; padding: 0; background-color: #333; width: 100%;}';
+    }
+    else{
+        var sf1 ='.footer-bar{list-style-type: none; margin: 0; padding: 0; background-color: #333; width: 100%; position: fixed; bottom: 0;}';
+    }
+    var sf2 ='.footer-element{display: inline-block; padding: 8px;}';
+    var sf3 ='.footer-element a{color: white; text-decoration: none;}';
+    var sf4 ='.footer-element a:hover{}';
     s.insertAdjacentHTML('beforeend', sf1 + sf2 + sf3 + sf4);
+    
 
 
 }
