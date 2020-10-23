2f037ae89ad4d9643d2099c9d9b80c86a154b90d
djinn0 <dsr.1991@gmail.com>
2020-10-22 22:18:46 -0400

    updates

--- navBarSelector.js
+++ navBarSelector.js
@@ -51,8 +51,8 @@ function generateSyling()
     var ss6 = 'input:checked + .slider:before { transform: translateX(16px); }'
 
     /* Styling of mobile Navigation Bar (sm = Styling Mobile) */
-    var sm1 = '.navigation-bar-mobile{ list-style-type: none; margin: 0; padding: 0; width: 100%; z-index: 11;}';
-    var sm2 = '.navigation-element-mobile{ padding: 8px; background-color: #333;  text-align: center; }'; 
+    var sm1 = '.navigation-bar-mobile{ list-style-type: none; margin: 0; padding: 0; width: 100%; z-index: 11; }';
+    var sm2 = '.navigation-element-mobile{ padding: 8px; background-color: #333;  text-align: center;  }'; 
     var sm3 = '.navigation-element-mobile a{ text-decoration: none; color: white; }';
     var sm4 = '.navigation-element-mobile:hover{ background-color: #111; padding: 8px; }';
     var sm5 = '.con{ padding-right: 8px; }' 
