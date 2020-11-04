12f135c0e2a08c21d62af4322c1631b2a61a9f67
djinn0 <dsr.1991@gmail.com>
2020-11-04 18:33:34 -0500

    update styling of toggle switch

--- navBarSelector.js
+++ navBarSelector.js
@@ -44,11 +44,12 @@ function generateSyling()
 
     /* Styling of Sliding Switch */
     var ss1 = '.switch {position: relative; vertical-align: -12px; display: inline-block; width: 36px; height: 20px; } ' 
-    var ss2 = '.Mswitch {position: relative; vertical-align: -2px; display: inline-block; width: 36px; height: 20px; } ' /* mobile slider */
-    var ss3 = '.slider {position: absolute; cursor: pointer; top: 0; left: 0; right: 0; bottom: 0; background-color: #CCC; transition: .4s; border-radius: 34px;}'
-    var ss4 = '.slider:before {position: absolute; content: ""; height: 16px; width: 16px; left: 2px; bottom: 2px; background-color: white; transition: .4s; border-radius: 50%;}'
-    var ss5 = 'input:checked + .slider { background-color: #2196F3; }'
-    var ss6 = 'input:checked + .slider:before { transform: translateX(16px); }'
+    var ss2 = '.Mswitch {position: relative; vertical-align: -16px; display: inline-block; width: 36px; height: 20px; } ' /* mobile slider */
+    var ss3 = '.Mswitch input{opacity: 0; width: 0; height: 0;}'
+    var ss4 = '.slider {position: absolute; cursor: pointer; top: 0; left: 0; right: 0; bottom: 0; background-color: #CCC; transition: .4s; border-radius: 34px;}'
+    var ss5 = '.slider:before {position: absolute; content: ""; height: 16px; width: 16px; left: 2px; bottom: 2px; background-color: white; transition: .4s; border-radius: 50%;}'
+    var ss6 = 'input:checked + .slider { background-color: #2196F3; }'
+    var ss7 = 'input:checked + .slider:before { transform: translateX(16px); }'
 
     /* Styling of mobile Navigation Bar (sm = Styling Mobile) */
     var sm1 = '.navigation-bar-mobile{ list-style-type: none; margin: 0; padding: 0; width: 100%; z-index: 11; }';
@@ -66,7 +67,7 @@ function generateSyling()
     var sn6 = ' #dropdown:hover .dropdown-content {display: block;}'; /*display menu content on hover */
     var sn7 = ' .dropdown-content a{display: block;}';
     /* insert styling */
-    s.insertAdjacentHTML('beforeend', sn1 + sn2 + sn3 + sn4 + sn5 + sn6 + sn7 + sm1 + sm2 + sm3 + sm5 + ss1 + ss2 + ss3 + ss4 + ss5 + ss6); 
+    s.insertAdjacentHTML('beforeend', sn1 + sn2 + sn3 + sn4 + sn5 + sn6 + sn7 + sm1 + sm2 + sm3 + sm5 + ss1 + ss2 + ss3 + ss4 + ss5 + ss6 + ss7); 
 
 }
 
